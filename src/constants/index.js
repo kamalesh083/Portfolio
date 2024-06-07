a4328fbf2ce7d57431a5a4b3f62486b9f967a370
kamalesh083 <144986148+kamalesh083@users.noreply.github.com>
2024-06-07 19:34:08 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -70,7 +70,7 @@ export const PROJECTS = [
 ];
 
 export const CONTACT = {
-  address: "767 Fifth Avenue, New York, NY 10153 ",
-  phoneNo: "+12 4555 666 00 ",
-  email: "me@example.com",
+  address: "Chennai, TamilNadu, India ",
+  phoneNo: "+91 90876 54321 ",
+  email: "kamaleshag96@gmail.com",
 };
